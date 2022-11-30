63b7d6c2011cce57551f8c27fa0f6df47e7a766d
Waleejah Rana <wrana@dons.usfca.edu>
2022-11-30 02:43:04 +0000

    Change back to original scripts

--- code/01_download_fastq.sh
+++ code/01_download_fastq.sh
@@ -16,8 +16,6 @@ set -euo pipefail
 OUTPUT_DIR="/data/sars_vcf_analysis/01_raw_fastq"
 TEMP_DIR="/data/sars_vcf_analysis/00_fasterq_temp"
 
-[1] "C:/Home/Waleejahrana-bioinformatics-final-project/data/00_sra_runtable/SraRunTable_PRJNA669945.txt"
-
 # script takes a single argument, which should be an SRA run table file
 if [ $# -ne 1 ]
 then
@@ -28,19 +26,19 @@ fi
 
 # download fastq files for all SRA runs in runtable file, assuming first column
 # and skipping first row as header
-for 669945 in $(tail -n +2 "$1" | cut -d, -f1)
+for run_id in $(tail -n +2 "$1" | cut -d, -f1)
 do
     # the -L 6 is the output level, 6 is the highest for lots of output
     # this also sets a temp directory on the RAID drive in case it is needed
     # this will also split forward and reverse reads if they would otherwise be
     # in a single file
-    if [ ! -f "${TEMP_DIR}"/"${669945}"/"${669945}".sra ]
+    if [ ! -f "${TEMP_DIR}"/"${run_id}"/"${run_id}".sra ]
     then
-            prefetch -p -O "$TEMP_DIR" "$669945"
+            prefetch -p -O "$TEMP_DIR" "$run_id"
     fi
-    if [ ! -f "${OUTPUT_DIR}/${669945}"_1.fastq ]
+    if [ ! -f "${OUTPUT_DIR}/${run_id}"_1.fastq ]
     then
-        fastq-dump --split-files -L 6 --outdir "$OUTPUT_DIR" "${TEMP_DIR}"/"${669945}"/"${669945}".sra
+        fastq-dump --split-files -L 6 --outdir "$OUTPUT_DIR" "${TEMP_DIR}"/"${run_id}"/"${run_id}".sra
     fi
 done
 
